026dbe40a774b4db86ee213894d73b92c5201e1b
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2016-06-12 15:37:17 -0500

    Fill the cheque view with dummy data

--- src/Cheque/Components/Cheque.elm
+++ src/Cheque/Components/Cheque.elm
@@ -4,8 +4,8 @@ import Html exposing (Html, div)
 import Html.Attributes exposing (style)
 import Cheque.Model exposing (Model)
 import Cheque.Update exposing (Msg)
-import Svg exposing (Svg, svg, rect, text', text, foreignObject, defs, g, line)
-import Svg.Attributes as Attributes exposing (id, fill, height, width, x, y, viewBox, fontFamily, strokeWidth, fontSize, stroke, x1, y1, x2, y2, fontWeight, strokeOpacity)
+import Svg exposing (Svg, svg, rect, text', text, foreignObject, defs, g, line, tspan)
+import Svg.Attributes as Attributes exposing (id, fill, height, width, x, y, viewBox, fontFamily, strokeWidth, fontSize, stroke, x1, y1, x2, y2, fontWeight, strokeOpacity, baselineShift, textAnchor)
 import Styles.Background as Background
 
 
@@ -46,6 +46,7 @@ date model =
     g [ id "date" ]
         [ text' [ x "450", y "90" ] [ text "DATE" ]
         , line [ x1 "490", y1 "90", x2 "620", y2 "90", stroke "black", strokeWidth "1px" ] []
+        , text' [ x "515", y "85", fontFamily "monospace" ] [ text "06/10/2016" ]
         ]
 
 
@@ -64,6 +65,7 @@ payableTo model =
         [ text' [ x "35", y "120" ] [ text "PAY TO THE" ]
         , text' [ x "35", y "135" ] [ text "ORDER OF" ]
         , line [ x1 "105", y1 "135", x2 "540", y2 "135", strokeWidth "1px", stroke "black" ] []
+        , text' [ x "115", y "130", fontFamily "monospace" ] [ text "Some Company Name" ]
         ]
 
 
@@ -72,7 +74,7 @@ amount model =
     g [ id "amount" ]
         [ text' [ x "550", y "131", fontSize "18px" ] [ text "$" ]
         , rect [ x "565", y "115", width "100", height "20", strokeWidth "1px", stroke "black", fill "white", strokeOpacity "0.5" ] []
-        , text' [ x "605", y "170" ] [ text "DOLLARS" ]
+        , text' [ x "660", y "130", textAnchor "end", fontFamily "monospace" ] [ text "1,550.00" ]
         ]
 
 
@@ -80,6 +82,13 @@ amountWords : Model -> Svg msg
 amountWords model =
     g [ id "amountWords" ]
         [ line [ x1 "35", y1 "170", x2 "600", y2 "170", strokeWidth "1px", stroke "black" ] []
+        , text' [ x "45", y "165", fontFamily "monospace" ]
+            [ text "ONE THOUSAND FIVE HUNDREDS AND FIFTY "
+            , tspan [ baselineShift "super", fontSize "8px" ] [ text "0" ]
+            , text "/"
+            , tspan [ baselineShift "-25%", fontSize "8px" ] [ text "100" ]
+            ]
+        , text' [ x "605", y "170" ] [ text "DOLLARS" ]
         ]
 
 
@@ -107,6 +116,6 @@ name model =
 address : Model -> Svg msg
 address model =
     g [ id "address" ]
-        [ text' [ x "30", y "50" ] [ text "886 Wood Ave" ]
-        , text' [ x "30", y "65" ] [ text "Kirkwood, MO 63122" ]
+        [ text' [ x "30", y "50" ] [ text "13 Elm St." ]
+        , text' [ x "30", y "65" ] [ text "St. Louis, MO 63112" ]
         ]
